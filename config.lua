3188ec5609981ecefe7e5ea29b1bc670ec220672
TweakyGaming <tweakymedia@gmail.com>
2022-03-16 10:25:57 +0100

    test

--- config.lua
+++ config.lua
@@ -3,6 +3,18 @@ Config = {}
 Config.PoliceOpen = true -- Can Police open all stashes
 
 Config.Stashes = {
+    ["cartelExtraStash"] = {
+        stashName = "Cartel Stash",
+        coords = vector3(-1791.55, 421.26, 125.21), 
+        requirecid = false,
+        jobrequired = false,
+        gangrequired = true,
+        gang = "cartel",
+        job = "",
+        cid = {},  
+        stashSize = 1250000,
+        stashSlots = 125, 
+    },
     -- ["geminimansion"] = {
     --     stashName = "geminimansion",
     --     coords = vector3(-1730.5, 358.6, 88.73), 
